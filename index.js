b6b1b8e4d63c5153ca6b7452aee35393b4ee1b9f
Aayush <aayushsharma47163@gmail.com>
2024-01-19 22:33:40 +0530

    Header Parser APi created

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var {ip} = require('address');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
@@ -24,7 +23,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = ip();
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
